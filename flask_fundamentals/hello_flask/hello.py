a9c248c8f261ae4a011af2b54e147168f1618a89
Kristen Martino <pikap3w@Kristens-MacBook-Pro.local>
2018-10-05 11:45:58 -0500

    flask fundamentals

--- flask_fundamentals/hello_flask/hello.py
+++ flask_fundamentals/hello_flask/hello.py
@@ -1,16 +1,5 @@
-# -*- coding: utf-8 -*-
-"""Example Google style docstrings.
+from flask import Flask, render_template  # Import Flask to allow us to create our app.
 
-This module demonstrates documentation as specified by the `Google Python
-Style Guide`_. Docstrings may extend over multiple lines. Sections are created
-with a section header and a colon followed by a block of indented text.
-
-.. _Google Python Style Guide:
-   https://github.com/google/styleguide/blob/gh-pages/pyguide.md
-
-"""
-
-from flask import Flask  # Import Flask to allow us to create our app.
 app = Flask(__name__)
 # Global variable __name__ tells Flask whether or not we are running the file
 # directly, or importing it as a module.
@@ -18,23 +7,22 @@ app = Flask(__name__)
 print(__name__)  # Just for fun, print __name__ to see what it is
 
 
-@app.route('/')
+@app.route("/")
 # The @ symbol designates a 'decorator' which attaches the following
 # function to the '/' route. This means that whenever we send a request to
 # localhost:5000/ we will run the following 'hello_world' function.
 def hello_world():
-    """Return the string 'Hello World!' as a response."""
-    return 'Hello World!'
+    return render_template("index.html", name="Jay")  # Render the template and return it!
 
 
-@app.route('/success')
+@app.route("/success")
 def success():
     """Return the string 'success' as a response."""
     return "success"
 
 
 @app.route(
-    '/hello/<name>'
+    "/hello/<name>"
 )  # for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
 def hello(name):
     """Return the string 'hello' + variable 'name' as a response."""
@@ -43,7 +31,7 @@ def hello(name):
 
 
 @app.route(
-    '/users/<username>/<id>'
+    "/users/<username>/<id>"
 )  # for a route '/users/____/____', two parameters in the url get passed as username and id
 def show_user_profile(username, id):
     print(username)
--- flask_fundamentals/hello_flask/understanding_routing.py
+++ flask_fundamentals/hello_flask/understanding_routing.py
@@ -1,27 +1,29 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
     return "Hello World!"
 
 
-@app.route('/dojo')
+@app.route("/dojo")
 def dojo():
     return "Dojo!"
 
 
-@app.route('/say/<name>')
+@app.route("/say/<name>")
 def say(name):
     if name == "john":
         return "Hi " + name.title() + "!"
     return "Hi " + name.title()
 
 
-@app.route('/repeat/<num>/<string>')
+@app.route("/repeat/<num>/<string>")
 def repeat(num, string):
     return string * int(num)
+
 
 if __name__ == "__main__":
     # If __name__ is "__main__" we know we are running this file directly and
