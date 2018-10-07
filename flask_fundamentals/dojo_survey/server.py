b932324d9ef8d87a97211a6a74482348bb92800e
Kristen <pikap3w@kristens-mbp.home>
2018-10-07 16:47:42 -0500

    renaming directories

---
+++ flask_fundamentals/dojo_survey/server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, redirect, request, session, flash
+
+app = Flask(__name__)
+app.secret_key = "Shhhhhhhh"
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/result", methods=["POST"])
+def submit():
+    print(request.form)
+    print('Name', request.form['name'])
+    print('Dojo Location', request.form['location'])
+    print('Favorite Language', request.form['language'])
+    print('Comment', request.form['comment'])
+    return render_template("result.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
