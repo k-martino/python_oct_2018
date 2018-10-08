705f40f7b994c741ba627cf6ba2b5630dba06819
Kristen <43310704+k-martino@users.noreply.github.com>
2018-10-07 22:23:09 -0500

    html table & minor changes to checkerboard

--- flask_fundamentals/checkerboard/server.py
+++ flask_fundamentals/checkerboard/server.py
@@ -10,6 +10,3 @@ def checkerboard(x, y):
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# http://localhost:5000 - should display 8 by 8 checkerboard
-# http://localhost:5000/(x)/(y) - should display x by y checkerboard.  For example, http://localhost:5000/10/10 should display 10 by 10 checkerboard.  Before you pass x or y to Jinja, please remember to convert it to integer first (so that you can use x or y in a for loop)
