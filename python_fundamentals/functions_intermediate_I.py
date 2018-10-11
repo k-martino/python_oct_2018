96e9a0b9938b61c178ddf3b99833d82726760388
Kristen <43310704+k-martino@users.noreply.github.com>
2018-10-10 21:17:46 -0500

    adding some early python assignments

--- python_fundamentals/functions_intermediate_I.py
+++ python_fundamentals/functions_intermediate_I.py
@@ -1,3 +1,9 @@
+def becheerful(str="", repeat=1):
+    print(f"good {str}" * repeat)
+
+
+becheerful(str="morning!", repeat=98)
+
 # As part of this assignment, please create a function randInt() where
 
 # randInt() returns a random integer between 0 to 100
@@ -7,18 +13,13 @@
 # Create this function without using random.randInt() but you are allowed to use random.random().
 
 
-
-# helpful tips for the next assignment
 import random
-# print(random.random()) # returns a random floating number between 0.000 to 1.000
-# print(random.random()*50) # returns a float between 0.000 to 50.000
-# int( 3.654 ) # returns 3
-# round( 3.654 ) # returns 4
 
-def randInt(min='', max=''):
-    print(int(random.random()*100))
 
-randInt()
-# randInt(max=50)
-# randInt(min=50)
-# randInt(min=50, max=500)
+def randInt(min=0, max=100):
+    print(int(random.random() * (max - min)) + min)
+
+
+randInt(min=50, max=500)
+randInt(min=50)
+randInt(max=50)
