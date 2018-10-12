49db4b9eca1931f367e7d27775237dff2e1540d0
Kristen <43310704+k-martino@users.noreply.github.com>
2018-10-12 14:44:31 -0500

    django survey

--- Django_intro/survey/apps/surveys/views.py
+++ Django_intro/survey/apps/surveys/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
+def index(request):
+    return render(request, "surveys/index.html")
+
+
+def process(request):
+    if request.method == "POST":
+        print("*" * 50)
+        print(request.POST)
+        request.session["name"] = request.POST["name"]
+        request.session["location"] = request.POST["location"]
+        request.session["language"] = request.POST["language"]
+        request.session["comment"] = request.POST["comment"]
+        return redirect("/result")
+    else:
+        return redirect("/")
+
+
+def result(request):
+    return render(request, "surveys/result.html")
