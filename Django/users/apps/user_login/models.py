ae8973c432ebdc729edec06db27948e618761669
Kristen <43310704+k-martino@users.noreply.github.com>
2018-10-15 13:51:52 -0500

    user_login

--- Django/users/apps/user_login/models.py
+++ Django/users/apps/user_login/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 
 # Create your models here.
-class User(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email_address = models.EmailField(max_length=255)
+class Users(models.Model):
+    first_name = models.CharField(max_length=255, validators=[MinLengthValidator(2)])
+    last_name = models.CharField(max_length=255, validators=[MinLengthValidator(2)])
+    email_address = models.EmailField(max_length=255, unique=True)
     age = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return "<Users object: {} {} {} {}>".format(self.first_name, self.last_name, self.email_address, self.age)
--- Django/users/users/settings.py
+++ Django/users/users/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.user_login',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
