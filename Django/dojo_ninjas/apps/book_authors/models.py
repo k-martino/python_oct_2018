3281caf4eeb8461cc6cf3a5ddec1288232322a1c
Kristen <43310704+k-martino@users.noreply.github.com>
2018-10-15 17:39:24 -0500

    books/authors assignment

---
+++ Django/dojo_ninjas/apps/book_authors/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    desc = models.TextField(max_length=1000, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=255, null=True)
+    last_name = models.CharField(max_length=255, null=True)
+    email = models.EmailField(max_length=255, null=True)
+    notes = models.TextField(null=True)
+    books = models.ManyToManyField(Book, related_name="authors")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
