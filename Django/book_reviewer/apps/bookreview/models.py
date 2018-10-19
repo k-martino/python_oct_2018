185fec6c7cd512108c11dee96b164f9f9ab56c3d
Kristen <43310704+k-martino@users.noreply.github.com>
2018-10-19 10:02:32 -0500

    adding more django project files

--- Django/book_reviewer/apps/bookreview/models.py
+++ Django/book_reviewer/apps/bookreview/models.py
@@ -1,1 +1,112 @@
-# Create your models here.
+import re
+
+import bcrypt
+from django.db import models
+
+EMAIL_REGEX = re.compile(r"^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$")
+
+
+class UserManager(models.Manager):
+    @staticmethod
+    def user_validator(post_data):
+        errors = {}
+        # Check for length
+        if len(post_data["first_name"]) < 2:
+            errors["first_name_len"] = "First name should have at least 2 characters"
+        if len(post_data["last_name"]) < 2:
+            errors["last_name_len"] = "Last name should have at least 2 characters"
+        if len(post_data["alias"]) < 2:
+            errors["alias_len"] = "Alias should have at least 2 characters"
+        if len(post_data["email"]) < 2:
+            errors["email_len"] = "Email should have at least 2 characters"
+        if len(post_data["password"]) < 8:
+            errors["password_len"] = "Password should have at least 8 characters"
+        # Make sure names are only letters
+        if not post_data["first_name"].isalpha():
+            errors["first_name_alpha"] = "First name should only contain letters"
+        if not post_data["last_name"].isalpha():
+            errors["last_name_alpha"] = "Last name should only contain letters"
+        # Make sure email matches format
+        if not EMAIL_REGEX.match(post_data["email"]):
+            errors["email_format"] = "Invalid email format"
+        # Make sure email isn't already in the list
+        if User.objects.filter(email=post_data["email"]):
+            errors["email_used"] = "Email already in use"
+        # Make sure both passwords match
+        if post_data["pw"] != post_data["pw_confirm"]:
+            errors["pw_match"] = "Passwords do not match"
+        return errors
+
+    @staticmethod
+    def login_validator(post_data):
+        errors = {}
+        # Check if email is in the database
+        if not User.objects.filter(email=post_data["email"]):
+            errors["email_db_check"] = "Invalid credentials"
+        # Check for correct password
+        else:
+            log_user = User.objects.filter(email=post_data["email"])[0]
+            if not bcrypt.checkpw(post_data["pw"].encode(), log_user.password.encode()):
+                errors["pw_db_check"] = "Invalid credentials"
+        return errors
+
+
+class BookManager(models.Manager):
+    @staticmethod
+    def book_validator(post_data):
+        errors = {}
+        # Checking for length in book title and book author
+        if len(post_data["title"]) < 2:
+            errors["title"] = "Title should have at least 2 characters"
+        if post_data["author"] == "add":
+            if len(post_data["new_author"]) < 2:
+                errors["author"] = "Author name should have at least 2 characters"
+        if len(post_data["review"]) < 10:
+            errors["review"] = "Review should have at least 10 characters"
+        return errors
+
+
+class ReviewManager(models.Manager):
+    @staticmethod
+    def review_validator(post_data):
+        errors = {}
+        if len(post_data["review"]) < 2:
+            errors["review"] = "Review should have at least 2 characters"
+        return errors
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    alias = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    pw_hash = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    uploaded_by = models.ForeignKey(User, related_name="uploaded_authors")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, related_name="authored_books")
+    uploaded_by = models.ForeignKey(User, related_name="uploaded_books")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = BookManager()
+
+
+class Review(models.Model):
+    content = models.TextField()
+    reviewer = models.ForeignKey(User, related_name="reviews")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    book = models.ForeignKey(Book, related_name="reviews")
+    rating = models.IntegerField()
+    objects = ReviewManager()
--- Django/book_reviewer/apps/bookreview/views.py
+++ Django/book_reviewer/apps/bookreview/views.py
@@ -1,1 +1,94 @@
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import User, Author, Book, Review
+from django.contrib import messages
+import bcrypt
+
+
+# render
+def index(request):
+    return render(request, "bookreview/index.html")
+
+
+def home(request):
+    return render(request, "/")
+
+
+def add_bk(request):
+    return render(request, "/")
+
+
+def bk(request):
+    return render(request, "/")
+
+
+def usr(request):
+    return render(request, "/")
+
+
+# process & redirect
+def register(request):
+    errors = User.objects.user_validator(request.POST)
+    if len(errors):
+        for key, value in errors.items():
+            messages.error(request, value, extra_tags="signup_errors")
+        return redirect("/")
+    else:
+        user = User.objects.create(
+            first_name=request.POST["first_name"],
+            last_name=request.POST["last_name"],
+            alias=request.POST["alias"],
+            email=request.POST["email"],
+            pw_hash=bcrypt.hashpw(request.POST["password"].encode(), bcrypt.gensalt()),
+        )
+        request.session["user_id"] = user.id
+        request.session["logged_in"] = True
+        return redirect("bookreview:home")
+
+
+def login(request):
+    errors = User.objects.login_validator(request.POST)
+    if len(errors):
+        for key, value in errors.items():
+            messages.error(request, value, extra_tags="login_errors")
+        return redirect("/")
+    else:
+        user = User.objects.get(email=request.POST["email"])
+        request.session["user_id"] = user.id
+        request.session["logged_in"] = True
+        return redirect("bookreview:home")
+
+
+def logout(request):
+    request.session.clear()
+    return redirect("/")
+
+
+def process_bk(request):
+    if request.method == "POST":
+        errors = Book.objects.book_validator(request.POST)
+        if len(errors):
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect("bookreview:add_bk")
+        else:
+            title = request.POST["title"]
+            user = User.objects.get(id=request.session["user_id"])
+            rating = int(request.POST["rating"])
+            if request.POST["author"] == "Other":
+                author = Author.objects.create(name=request.POST("new_author"), uploaded_by=user)
+            else:
+                author = Author.objects.filter(name=request.POST["author"])[0]
+            book = Book.objects.create(title=title, author=author, uploaded_by=user)
+            Review.objects.create(
+                content=request.POST["review"], reviewer=user, book=book, rating=rating
+            )
+            request.session["book"] = book.id
+            return redirect("view_bk", book_id=book.id)
+
+
+def process_rev(request):
+    return redirect("/")
+
+
+def destroy(request):
+    return redirect("/")
